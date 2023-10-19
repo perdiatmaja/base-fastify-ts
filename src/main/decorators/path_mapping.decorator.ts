4582e3afd1e9e74fd68394600dad8dcb7f8544ac
Perdi Atmaja <perdi.atmaja@outlook.com>
2023-10-19 19:19:12 +0700

    Revert custom constructor changes

--- src/main/decorators/path_mapping.decorator.ts
+++ src/main/decorators/path_mapping.decorator.ts
@@ -1,18 +1,6 @@
-import BaseRouter from "../modules/app/base.router"
-
-function customConstructor(constructor: any): any {
-    function newConstructor(...args: any[]) {
-        return new constructor(...args)
-    }
-
-    newConstructor.prototype = constructor.prototype
-
-    return newConstructor
-}
-
 function PathMapping(mainPath: string) {
     return function (target: any) {
-        const decoratedTarget = customConstructor(target)
+        const decoratedTarget = target
         decoratedTarget.prototype["mainPath"] = mainPath
         return decoratedTarget
     }
