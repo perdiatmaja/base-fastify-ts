fd653c2048390c00290680a299dc52875f449ffd
Perdi Atmaja <perdi.atmaja@outlook.com>
2023-10-23 04:23:14 +0700

    Introduce new file to ignore files from npm

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export * from "./src/main/decorators/method"
+export * from "./src/main/decorators/parameters"
+export { PathMapping } from "./src/main/decorators/path_mapping.decorator"
+export { Application as SpringifyApp } from "./src/main/application"
