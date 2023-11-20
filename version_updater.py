51eedd0f00da38cccabb2814c547cd46921dcfa7
Perdi Mardhothillah Jaka Atmaja <perdi.atmaja@outlook.com>
2023-11-20 15:11:27 +0700

    Merge back develop (#4)
    * Update version to 0.0.2-SNAPSHOT
    
    * Update version to
    
    * Improve version updater
    
    * Uncomment script install and build in publish.sh
    
    * Improve version updater with json pretty print
    
    * Update reamde.md
    
    * Update version to 0.0.1-SNAPSHOT

--- version_updater.py
+++ version_updater.py
@@ -3,33 +3,52 @@ import sys
 import re
 
 branch = sys.argv[1]
-RELEASE_REGEX="^(release)+(\/\w\.\w\.\w)$"
+RELEASE_REGEX = "^(release)+(\/\w\.\w\.\w)$"
+SNAPSHOT = "SNAPSHOT"
+RELEASE = "RELEASE"
 
-isRelease=re.search(RELEASE_REGEX, branch)
+isRelease = re.search(RELEASE_REGEX, branch)
 
-if (not isRelease and branch != "develop"):
+if not isRelease and branch != "develop":
     sys.exit()
 
-packageFile = open('install_package.json')
-packageData = json.load(packageFile) 
+packageFile = open("package.json")
+packageData = json.load(packageFile)
 packageFile.close()
+versionHistory = {}
+versionHistoryFile = open("version_history", "r")
 
-version = branch.replace("release/", "") if (isRelease) else packageData["version"]
+for data in versionHistoryFile:
+    dataArr = data.replace("\n", "").split("=")
+    versionHistory[dataArr[0]] = dataArr[1]
 
-if (isRelease):
-    packageData["version"] = f'{version}'
-elif (branch == "develop"):
+versionHistoryFile.close()
+
+version = branch.replace("release/", "") if (isRelease) else str(packageData["version"])
+
+if isRelease and version != versionHistory[RELEASE]:
+    packageData["version"] = f"{version}"
+
+elif branch == "develop":
+    version = version.replace(f"-{SNAPSHOT}", "")
     versionArr = version.split(".")
-    buildNo = int(versionArr[len(versionArr)-1])+1
-    version = f'{versionArr[0]}.{versionArr[1]}.{buildNo}'
-    packageData["version"] = f'{version}-SNAPSHOT'
-
-with open("install_package.json", "w") as outfile:
-    json.dump(packageData, outfile)
-
-del packageData["scripts"]
+    buildNo = int(versionArr[len(versionArr) - 1]) + 1
+    version = (
+        f"{versionArr[0]}.{versionArr[1]}.{buildNo}-{SNAPSHOT}"
+        if version == versionHistory[SNAPSHOT]
+        else f"{version}-{SNAPSHOT}"
+    )
+    packageData["version"] = f"{version}"
+    versionHistory[SNAPSHOT]=version
 
 with open("package.json", "w") as outfile:
-    json.dump(packageData, outfile)
-    
+    json.dump(packageData, outfile, indent=4)
+
+versionHistoryFile = open("version_history", "w")
+
+for key in versionHistory:
+    versionHistoryFile.writelines(f"{key}={versionHistory[key]}\n")
+
+versionHistoryFile.close()
+
 print(version)
