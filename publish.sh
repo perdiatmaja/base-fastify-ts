fade4687119b1313b261586210cc8fa7835d7db9
Perdi Mardhothillah Jaka Atmaja <perdi.atmaja@outlook.com>
2023-11-20 07:27:58 +0700

    Release/0.0.1 (#3)
    * Fix publish script to publish only to release if the branch is release
    
    * Uncomment git push and npm publish on publish.sh

--- publish.sh
+++ publish.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# ./install.sh
-# ./build.sh
+./install.sh
+./build.sh
 
-MAIN="main"
+RELEASE="^(release)+(\/\w\.\w\.\w)$"
 DEVELOP="develop"
 
 branch_name=$(git symbolic-ref -q HEAD);
 branch_name=${branch_name##refs/heads/};
 branch_name=${branch_name:-HEAD};
 
-if [ $branch_name == $MAIN ] || [ $branch_name == $DEVELOP ];
+if [[ $branch_name =~ $RELEASE ]] || [ $branch_name == $DEVELOP ];
 then
     version=$(python3 version_updater.py $branch_name)
+    echo $version
     git add .
     git commit -m "Update version to $version"
     git push
     npm publish
 fi
+
+./clear.sh
