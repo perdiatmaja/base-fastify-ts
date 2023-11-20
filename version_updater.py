2fa02cd70b26dce9c501e029e14b61079012ba0b
Perdi Atmaja <perdi.atmaja@outlook.com>
2023-11-20 15:47:28 +0700

    Fix version updater

--- version_updater.py
+++ version_updater.py
@@ -15,39 +15,42 @@ if not isRelease and branch != "develop":
 packageFile = open("package.json")
 packageData = json.load(packageFile)
 packageFile.close()
-versionHistory = {}
+versionHistories = {}
 versionHistoryFile = open("version_history", "r")
 
 for data in versionHistoryFile:
     dataArr = data.replace("\n", "").split("=")
-    versionHistory[dataArr[0]] = dataArr[1]
+    versionHistories[dataArr[0]] = dataArr[1]
 
 versionHistoryFile.close()
 
 version = branch.replace("release/", "") if (isRelease) else str(packageData["version"])
 
-if isRelease and version != versionHistory[RELEASE]:
+if isRelease and version != versionHistories[RELEASE]:
     packageData["version"] = f"{version}"
 
 elif branch == "develop":
     version = version.replace(f"-{SNAPSHOT}", "")
     versionArr = version.split(".")
     buildNo = int(versionArr[len(versionArr) - 1]) + 1
-    version = (
+    versionHistory = versionHistories[SNAPSHOT]
+
+    updatedVersion = (
         f"{versionArr[0]}.{versionArr[1]}.{buildNo}-{SNAPSHOT}"
-        if version == versionHistory[SNAPSHOT]
+        if f"{version}-{SNAPSHOT}" == versionHistory
         else f"{version}-{SNAPSHOT}"
     )
-    packageData["version"] = f"{version}"
-    versionHistory[SNAPSHOT]=version
+
+    packageData["version"] = f"{updatedVersion}"
+    versionHistories[SNAPSHOT] = updatedVersion
 
 with open("package.json", "w") as outfile:
     json.dump(packageData, outfile, indent=4)
 
 versionHistoryFile = open("version_history", "w")
 
-for key in versionHistory:
-    versionHistoryFile.writelines(f"{key}={versionHistory[key]}\n")
+for key in versionHistories:
+    versionHistoryFile.writelines(f"{key}={versionHistories[key]}\n")
 
 versionHistoryFile.close()
 
